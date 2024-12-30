eb9940cb4242fc5f859ae508533e200260eb8bd8
devuser1 <devuser1@vm-web-dev-01>
2024-12-30 08:15:15 +0000

    Chart of Account delete rework

--- QDERPWeb/Areas/Finance/Controllers/ChartOfAccountsController.cs
+++ QDERPWeb/Areas/Finance/Controllers/ChartOfAccountsController.cs
@@ -5,6 +5,7 @@ using QD.ERP.Web.DAL.Entities;
 namespace QD.ERP.Web.Areas.Finance.Controllers
 {
     [Route("api/[controller]/[action]")]
+    // [Route("Finapi/[controller]/[action]")]
     [ApiController]
     public class ChartOfAccountsController : Controller
     {
@@ -13,12 +14,32 @@ namespace QD.ERP.Web.Areas.Finance.Controllers
         {
             _context = context;
         }
-
         [HttpGet]
         public async Task<ActionResult> GetChartOfAccounts()
         {
 
             return Json(_context.Qry20107ChartOfAccounts.ToList());
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Delete(string accountId)
+        {
+            if (string.IsNullOrEmpty(accountId))
+            {
+                return BadRequest("Account ID is required.");
+            }
+
+            var item = _context.Tbl201ChartOfAccounts.FirstOrDefault(p => p.AccountId == accountId);
+            if (item != null)
+            {
+                _context.Tbl201ChartOfAccounts.Remove(item);
+                _context.SaveChanges();
+                return Ok(new { success = true });
+            }
+
+            return NotFound(new { success = false, message = "Account not found." });
         }
 
     }
