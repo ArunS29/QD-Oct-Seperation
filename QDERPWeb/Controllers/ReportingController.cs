e79d5f205c4bf665cc0bf208abc936db35feb5d9
Kaviyarasan Aruchamy <kaviyarasanaruchamy@gmail.com>
2025-02-04 15:31:34 +0530

    Design and libraries

--- QDERPWeb/Controllers/ReportingController.cs
+++ QDERPWeb/Controllers/ReportingController.cs
@@ -1,12 +1,25 @@
-﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
+﻿using DevExpress.AspNetCore.Reporting.QueryBuilder;
+using DevExpress.AspNetCore.Reporting.QueryBuilder.Native.Services;
+using DevExpress.AspNetCore.Reporting.ReportDesigner;
+using DevExpress.AspNetCore.Reporting.ReportDesigner.Native.Services;
 using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
-using Microsoft.AspNetCore.Mvc;
+using DevExpress.AspNetCore.Reporting.WebDocumentViewer.Native.Services;
 
-namespace QDERPWeb.Controllers
+
+public class CustomWebDocumentViewerController : WebDocumentViewerController
 {
-    public class CustomWebDocumentViewerController : WebDocumentViewerController
-    {
-        public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService)
-            : base(controllerService) { }
-    }
+    public CustomWebDocumentViewerController(IWebDocumentViewerMvcControllerService controllerService)
+        : base(controllerService) { }
+}
+
+public class CustomReportDesignerController : ReportDesignerController
+{
+    public CustomReportDesignerController(IReportDesignerMvcControllerService controllerService)
+        : base(controllerService) { }
+}
+
+public class CustomQueryBuilderController : QueryBuilderController
+{
+    public CustomQueryBuilderController(IQueryBuilderMvcControllerService controllerService)
+        : base(controllerService) { }
 }
