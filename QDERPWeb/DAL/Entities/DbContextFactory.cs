2ab4c2969a003494ed755b5fd1c7808fc72ff54e
devuser1 <devuser1@vm-web-dev-01>
2025-02-28 17:59:48 +0000

    sample DAL

---
+++ QDERPWeb/DAL/Entities/DbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QD.ERP.Web.DAL.Entities
+{
+    public class DbContextFactory
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DbContextFactory(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public ERPMasterWtDataContext CreateDbContext(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ERPMasterWtDataContext>();
+            optionsBuilder.UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            return new ERPMasterWtDataContext(optionsBuilder.Options);
+        }
+
+
+
+    }
+}
