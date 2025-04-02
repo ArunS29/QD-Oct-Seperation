759150481cda05d7f3489533350573952207aa8c
ELAVARASANPulseinfotech <elavarasan@PulseInfotechCo.onmicrosoft.com>
2025-04-02 17:01:57 +0530

    cost detail report

---
+++ QDERPWeb/Areas/Finance/Reports/Cost Analysis/Detailed Report/detailGroupBydate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace QD.ERP.Web.Areas.Finance.Reports.Cost_Analysis.Detailed_Report
+{
+	public partial class detailGroupBydate : DevExpress.XtraReports.UI.XtraReport
+	{	
+		public detailGroupBydate()
+		{
+			InitializeComponent();
+		}
+	}
+}
