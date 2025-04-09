c763a4cb52b70f6655ee6ca59b4468dbf65c4b6c
devuser1 <devuser1@vm-web-dev-01>
2025-04-09 08:25:49 +0000

    Currency save table

--- QDERPWeb/DAL/Entities/Tbl201VoucherMaster.cs
+++ QDERPWeb/DAL/Entities/Tbl201VoucherMaster.cs
@@ -70,5 +70,6 @@ public partial class Tbl201VoucherMaster
     public string ReferenceNote { get; set; }
 
     public string RentalPayslipNo { get; set; }
-  //  public object VoucherEntries { get; internal set; }
+    //  public object VoucherEntries { get; internal set; }
+    public int currencyid { get; set; }
 }
