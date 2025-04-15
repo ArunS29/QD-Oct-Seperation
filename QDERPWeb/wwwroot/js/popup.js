de9d518a000adb768ccc9b12788380c67d7884f0
sathish <132430112+sathish123momo@users.noreply.github.com>
2025-04-15 14:15:10 +0530

    popup globalization

---
+++ QDERPWeb/wwwroot/js/popup.js
@@ -0,0 +1,12 @@
+﻿// Function to open modal with URL and title
+function openModal(url, title) {
+    document.getElementById("modalIframe").src = url + "?modal=true"; // Append ?modal=true
+    document.getElementById("modalTitle").innerText = title; // Set modal title
+    document.getElementById("customModal").style.display = "block";
+}
+
+// Function to close modal
+function closeModal() {
+    document.getElementById("customModal").style.display = "none";
+    document.getElementById("modalIframe").src = ""; // Clear iframe on close
+}
