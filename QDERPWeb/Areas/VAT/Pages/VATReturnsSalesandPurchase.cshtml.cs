6f30e1950f1e9344d37c44937c265ef0a0cbceb9
Poovizhi <Poovizhi@PulseInfotechCo.onmicrosoft.com>
2025-04-22 11:41:43 +0530

    VAT returns sales and purchase

---
+++ QDERPWeb/Areas/VAT/Pages/VATReturnsSalesandPurchase.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QD.ERP.Web.Areas.VAT.Pages
+{
+    public class VATReturnsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
