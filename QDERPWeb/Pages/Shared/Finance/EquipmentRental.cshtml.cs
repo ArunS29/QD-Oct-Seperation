751997517beedcec2b7d00b025f69b531e58de51
Musharafcacthu <Mushraf@PulseInfotechCo.onmicrosoft.com>
2025-04-24 19:56:37 +0530

    user access ui

---
+++ QDERPWeb/Pages/Shared/Finance/EquipmentRental.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QD.ERP.Web.Pages.Shared.Finance
+{
+    public class EquipmentRentalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
