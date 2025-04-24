811452b5c2242712eb6e6d5e9c0cd93805dde42c
Arun@Pulse <Arunkumar@PulseInfotechCo.onmicrosoft.com>
2025-04-24 18:51:32 +0530

    With base currency voucher payment

--- QDERPWeb/Areas/Finance/Pages/VoucherBankPayment.cshtml.cs
+++ QDERPWeb/Areas/Finance/Pages/VoucherBankPayment.cshtml.cs
@@ -7,19 +7,10 @@ namespace QD.ERP.Web.Areas.Finance.Pages
 {
     public class VoucherBankPaymentModel : PageModel
     {
-        private readonly CurrencyService _currencyService;
-
-        public List<CurrencyMaster> Currencies { get; set; }
-
-        public VoucherBankPaymentModel(CurrencyService currencyService)
-        {
-            _currencyService = currencyService;
-        }
-
         public void OnGet()
         {
-            // Retrieve the list of currencies using the CurrencyService
-            Currencies = _currencyService.GetCurrencies();
+            
+       
         }
     }
 }
--- QDERPWeb/Areas/Finance/Pages/VoucherCashPayment.cshtml.cs
+++ QDERPWeb/Areas/Finance/Pages/VoucherCashPayment.cshtml.cs
@@ -8,19 +8,9 @@ namespace QD.ERP.Web.Areas.Finance.Pages
 {
     public class VoucherMasterModel : PageModel
     {
-        private readonly CurrencyService _currencyService;
-
-        public List<CurrencyMaster> Currencies { get; set; }
-
-        public VoucherMasterModel(CurrencyService currencyService)
-        {
-            _currencyService = currencyService;
-        }
-
         public void OnGet()
         {
-            // Retrieve the list of currencies using the CurrencyService
-            Currencies = _currencyService.GetCurrencies();
+       
         }
     }
 }
