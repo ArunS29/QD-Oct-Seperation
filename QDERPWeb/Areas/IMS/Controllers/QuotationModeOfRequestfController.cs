9edf36327404233f9fc3da86f2ad30408ba773e6
KowsalyaPulseInfotech <kowsalya@pulseinfotechco.onmicrosoft.com>
2025-05-06 11:45:22 +0530

    IMS-ProjectSubUnitsandQuotationModeRequestForm

---
+++ QDERPWeb/Areas/IMS/Controllers/QuotationModeOfRequestfController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QD.ERP.Web.DAL.Entities;
+using QD.ERP.Web.Service;
+
+namespace QD.ERP.Web.Areas.IMS.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class QuotationModeOfRequestfController : Controller
+    {
+        private readonly TenantDbContextHelper _tenantDbContextHelper;
+        private readonly ILogger<QuotationModeOfRequestfController> _logger;
+
+        public QuotationModeOfRequestfController(ILogger<QuotationModeOfRequestfController> logger, TenantDbContextHelper tenantDbContextHelper)
+        {
+            _tenantDbContextHelper = tenantDbContextHelper;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetModeofRequest()
+        {
+            try
+            {
+                if (_tenantDbContextHelper.TryGetTenantAndDbContext(out Tenant tenant, out ERPMasterWtDataContext dbContext))
+                {
+                    var dbSignatories = await dbContext.Tbl30103ModeOfRequestMasters
+                       .Select(s => new
+                       {
+                           s.ModeOfRequestId,
+                           s.ModeOfRequest
+                         
+                       })
+                        .ToListAsync();
+
+                    return Json(dbSignatories); // return raw data, paging/sorting done on client-side
+                }
+
+                return Unauthorized(new { message = "Invalid tenant.", success = false });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "An error occurred while loading data.", details = ex.Message });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveOrUpdateMode([FromBody] Tbl30103ModeOfRequestMaster model)
+        {
+            if (_tenantDbContextHelper.TryGetTenantAndDbContext(out Tenant tenant, out ERPMasterWtDataContext dbContext))
+            {
+                try
+                {
+                    var now = DateTime.Now;
+
+                    var existing = await dbContext.Tbl30103ModeOfRequestMasters
+                        .FirstOrDefaultAsync(x => x.ModeOfRequestId == model.ModeOfRequestId
+);
+
+                    if (existing != null)
+                    {
+                        existing.ModeOfRequest = model.ModeOfRequest;
+                      
+
+                    }
+                    else
+                    {
+                        // Assign new SignatoryId
+                        var lastId = await dbContext.Tbl30103ModeOfRequestMasters
+                            .OrderByDescending(x => x.ModeOfRequestId)
+                            .Select(x => x.ModeOfRequestId)
+                            .FirstOrDefaultAsync();
+
+                        model.ModeOfRequestId = (byte)(lastId + 1); // Assuming short type
+
+                        dbContext.Tbl30103ModeOfRequestMasters.Add(model);
+                    }
+
+                    await dbContext.SaveChangesAsync();
+
+                    return Ok(new { success = true, message = "Saved successfully", id = model.ModeOfRequestId });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError($"Error in SaveSignatory: {ex}");
+                    return StatusCode(500, new { success = false, message = ex.Message });
+                }
+            }
+
+            return Unauthorized(new { success = false, message = "Invalid tenant" });
+        }
+    }
+}
