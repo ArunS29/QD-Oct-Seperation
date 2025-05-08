ebe61f5ba700de16bfd5c580f7a2b909ab84494d
Sathish <Mushraf@PulseInfotechCo.onmicrosoft.com>
2025-05-08 18:31:52 +0530

    IMS Master Forms Functionality

---
+++ QDERPWeb/Areas/IMS/Controllers/SalesOrdersController.cs
@@ -0,0 +1,81 @@
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Humanizer;
+using DevExtreme.AspNet.Data.ResponseModel;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using QD.ERP.Web.Areas.Finance.Models;
+using QD.ERP.Web.Areas.Finance.Reports.Payable_Statements;
+using QD.ERP.Web.DAL.Entities;
+using QD.ERP.Web.Service;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QD.ERP.Web.Areas.IMS.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SalesOrdersController : Controller
+    {
+        private readonly TenantDbContextHelper _tenantDbContextHelper;
+        private readonly ILogger<SalesOrdersController> _logger;
+        public SalesOrdersController(ILogger<SalesOrdersController> logger, TenantDbContextHelper tenantDbContextHelper)
+        {
+            _tenantDbContextHelper = tenantDbContextHelper;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetSalesOrders(DateTime? fromDate, DateTime? toDate)
+        {
+            if (_tenantDbContextHelper.TryGetTenantAndDbContext(out Tenant tenant, out ERPMasterWtDataContext dbContext))
+            {
+                var query = dbContext.Qry60204salesOrderViewMasters.AsQueryable();
+
+
+                // Default dates if not provided
+                if (!fromDate.HasValue)
+                {
+                    fromDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1); // Start of the current month
+                }
+
+                if (!toDate.HasValue)
+                {
+                    toDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)); // End of the current month
+                }
+
+                // Filtering by date range
+                query = query.Where(i => i.SalesOrderDate >= fromDate && i.SalesOrderDate <= toDate);
+
+                // Fetching the data
+                var data = await query.Select(i => new
+                {
+                    i.SalesOrderNo,
+                    i.SalesOrderDate,
+                    i.ClientName,
+                    i.ClientPono,
+                    i.Mprno,
+                    i.QuoteNo,
+                    i.InvoiceStatus,
+                    i.OrderStatus,
+                    i.NoOfItems,
+                    i.TotalBeforeDiscount,
+                    i.Discount,
+                    i.TotalAfterDiscount,
+                    i.TotalTaxAmount,
+                    i.TotalWithTax,
+                }).ToListAsync();
+
+                return Json(data);
+            }
+
+            return Unauthorized(new { message = "Invalid tenant." });
+        }
+
+
+    }
+}
