2e3d899fbf5d3a88173a84f989f1eb276a921c0c
Sathish <Mushraf@PulseInfotechCo.onmicrosoft.com>
2025-05-08 14:51:13 +0530

    Delivery note master form

---
+++ QDERPWeb/Areas/Finance/Controllers/DeliveryNoteController.cs
@@ -0,0 +1,80 @@
+using DevExtreme.AspNet.Data;
+using DevExtreme.AspNet.Mvc;
+using Humanizer;
+using DevExtreme.AspNet.Data.ResponseModel;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using QD.ERP.Web.Areas.Finance.Models;
+using QD.ERP.Web.Areas.Finance.Reports.Payable_Statements;
+using QD.ERP.Web.DAL.Entities;
+using QD.ERP.Web.Service;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QD.ERP.Web.Areas.IMS.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DeliveryNoteController : Controller
+    {
+        private readonly TenantDbContextHelper _tenantDbContextHelper;
+        private readonly ILogger<DeliveryNoteController> _logger;
+
+        public DeliveryNoteController(ILogger<DeliveryNoteController> logger, TenantDbContextHelper tenantDbContextHelper)
+        {
+            _tenantDbContextHelper = tenantDbContextHelper;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetDeliveryNotes(DateTime? fromDate, DateTime? toDate)
+        {
+            if (_tenantDbContextHelper.TryGetTenantAndDbContext(out Tenant tenant, out ERPMasterWtDataContext dbContext))
+            {
+                var query = dbContext.Qry60304deliveryNoteViewMasters.AsQueryable();
+
+
+                // Default dates if not provided
+                if (!fromDate.HasValue)
+                {
+                    fromDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1); // Start of the current month
+                }
+
+                if (!toDate.HasValue)
+                {
+                    toDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)); // End of the current month
+                }
+
+                // Filtering by date range
+                query = query.Where(i => i.DeliveryDate >= fromDate && i.DeliveryDate <= toDate);
+
+                // Fetching the data
+                var data = await query.Select(i => new
+                {
+                    i.DeliveredTo,
+                    i.DeliveryNoteNo,
+                    i.DeliveryDate,
+                    i.DeliveryIssuedTo,
+                    i.Mprno,
+                    i.InvoiceNo,
+                    i.ClientName,
+                    i.ClientPono,
+                    i.ClientProject,
+                    i.SalesOrderNo,
+                    i.StoreName
+                }).ToListAsync();
+
+                return Json(data);
+            }
+
+            return Unauthorized(new { message = "Invalid tenant." });
+        }
+
+
+
+    }
+}
