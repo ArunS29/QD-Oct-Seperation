3ae5f25182bdf88504802e85b2bd95a6ae935e6f
Poovizhi <Poovizhi@PulseInfotechCo.onmicrosoft.com>
2025-05-21 18:03:03 +0530

    Grid UI fix and Localization

--- QDERPWeb/wwwroot/js/localization.js
+++ QDERPWeb/wwwroot/js/localization.js
@@ -2535,11 +2535,23 @@
         "Business Card 1":"Business Card 1",
         "Type new Quotation Status & Add to the Existing List:": "Type new Quotation Status & Add to the Existing List:",
         "Statement of Account Order By VocuherNo (Without Narration)": "Statement of Account Order By VocuherNo (Without Narration)",
+        "Type new Quotation Status & Add to the Existing List:": "Type new Quotation Status & Add to the Existing List:",
+        "Invoice Date:": "Invoice Date:",
+        "VAT Description": "VAT Description",
+        "Invoice Type (Arabic)": "Invoice Type (Arabic)",
+
     },
     "ar": {
         "Business Card 2": "بطاقة العمل 2",
         "Business Card 1":"بطاقة العمل 1",
         "Statement of Account Order By VocuherNo (Without Narration)":"طلب كشف حساب حسب رقم الفاتورة (بدون شرح)",
+       
+    //},
+    //"ar": {
+        
+    "VAT Description": "وصف ضريبة القيمة المضافة",
+
+        "Invoice Type (Arabic)": "نوع الفاتورة (بالعربية)",
         "Quotation Status:":"حالة الاقتباس:",
         "Quotation Status": "حالة الاقتباس",
         "Type new Quotation Status & Add to the Existing List:": "اكتب حالة عرض الأسعار الجديدة وأضفها إلى القائمة الموجودة:",
