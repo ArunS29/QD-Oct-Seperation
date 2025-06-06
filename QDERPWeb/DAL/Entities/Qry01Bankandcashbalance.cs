5104d049d2566f97cad139a712f8245e80356e13
Arun@Pulse <Arunkumar@PulseInfotechCo.onmicrosoft.com>
2025-06-06 19:46:42 +0530

    query optimized to dashboard

---
+++ QDERPWeb/DAL/Entities/Qry01Bankandcashbalance.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace QD.ERP.Web.DAL.Entities
+{
+    [Keyless]
+    public class Qry01Bankandcashbalance
+    {
+        public string AccountHeadName { get; set; }
+        public string AccountGroup { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
