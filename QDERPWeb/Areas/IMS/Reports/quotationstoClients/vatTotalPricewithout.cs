4bd08f203a95ced9be21cf02cef7931581b7ff24
KeerthanaMugi <Keerthana@PulseInfotechCo.onmicrosoft.com>
2025-06-18 10:27:53 +0530

    Ims Quotations Reports Amountinwords

--- QDERPWeb/Areas/IMS/Reports/quotationstoClients/vatTotalPricewithout.cs
+++ QDERPWeb/Areas/IMS/Reports/quotationstoClients/vatTotalPricewithout.cs
@@ -105,6 +105,11 @@ namespace QD.ERP.Web.Areas.IMS.Reports.quotationstoClients
             {
                 this.DataSource = dt;
                 this.DataMember = ""; // If you're using DataBinding directly, otherwise specify table name
+                                    
+                decimal totalAmount = Convert.ToDecimal(dt.Compute("SUM(TotalAfterDiscount)", ""));
+
+                if (FindControl("xrLabel2", true) is XRLabel labelEnglish)
+                    labelEnglish.Text = $"Amount in Words: {NumberToWordsHelper.ToEnglishWords(totalAmount)}";
             }
         }
 
@@ -144,6 +149,72 @@ namespace QD.ERP.Web.Areas.IMS.Reports.quotationstoClients
             }
 
             return dt;
+        }
+        public static class NumberToWordsHelper
+        {
+            public static string ToEnglishWords(decimal number)
+            {
+                var integer = (int)number;
+                var fraction = (int)((number - integer) * 100);
+
+                string result = NumberToWords(integer);
+
+                if (fraction > 0)
+                    result += " and " + NumberToWords(fraction);
+
+                return result + " Only";
+            }
+
+            private static string NumberToWords(int number)
+            {
+                if (number == 0)
+                    return "Zero";
+
+                if (number < 0)
+                    return "Minus " + NumberToWords(Math.Abs(number));
+
+                string words = "";
+
+                if ((number / 1000000) > 0)
+                {
+                    words += NumberToWords(number / 1000000) + " Million ";
+                    number %= 1000000;
+                }
+
+                if ((number / 1000) > 0)
+                {
+                    words += NumberToWords(number / 1000) + " Thousand ";
+                    number %= 1000;
+                }
+
+                if ((number / 100) > 0)
+                {
+                    words += NumberToWords(number / 100) + " Hundred ";
+                    number %= 100;
+                }
+
+                if (number > 0)
+                {
+                    var unitsMap = new[] {
+                 "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                 "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"
+             };
+                    var tensMap = new[] {
+                 "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+             };
+
+                    if (number < 20)
+                        words += unitsMap[number];
+                    else
+                    {
+                        words += tensMap[number / 10];
+                        if ((number % 10) > 0)
+                            words += "-" + unitsMap[number % 10];
+                    }
+                }
+
+                return words.Trim();
+            }
         }
     }
 }
