8285178228732506c40e078aad32b40e5d993762
Arun@Pulse <Arunkumar@PulseInfotechCo.onmicrosoft.com>
2025-07-05 18:56:22 +0530

    latestscript

---
+++ QDERPWeb/Service/TenantRouteModelConvention.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace QD.ERP.Web.Service
+{
+    // This convention prepends "{tenantName}/" to all Razor Page routes
+    public class TenantRouteModelConvention : IPageRouteModelConvention
+    {
+        public void Apply(PageRouteModel model)
+        {
+            foreach (var selector in model.Selectors)
+            {
+                var attributeRouteModel = selector.AttributeRouteModel;
+                if (attributeRouteModel != null && !attributeRouteModel.Template.StartsWith("{tenantName}/"))
+                {
+                    attributeRouteModel.Template = "{tenantName}/" + attributeRouteModel.Template;
+                }
+            }
+        }
+    }
+}
