caca1f6e9aa760ce9e05489167229e66bdf56815
Poovizhi <Poovizhi@PulseInfotechCo.onmicrosoft.com>
2025-07-22 18:15:38 +0530

    VAT Proforma Invoice Save functionality

---
+++ QDERPWeb/Pages/Shared/VAT/VATInvoiceWithDetailsFooter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QD.ERP.Web.Pages.Shared.VAT
+{
+    public class VATInvoiceWithDetailsFooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
