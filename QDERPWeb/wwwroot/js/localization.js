fefe6885a1e1655f11920d3945f18b6751c65954
Poovizhi <Poovizhi@PulseInfotechCo.onmicrosoft.com>
2025-07-24 04:33:47 +0530

    VAT Module UI and Bugs fixing

--- QDERPWeb/wwwroot/js/localization.js
+++ QDERPWeb/wwwroot/js/localization.js
@@ -2280,6 +2280,7 @@ var dictionary = {
         "GCC Countries": "GCC Countries",
         "Others Countries": "Others Countries",
         "Exports Revenue": "Exports Revenue",
+        "Exports":"Exports",
         "VAT Amount Classified": "VAT Amount Classified",
         "Domestic Standard Rated Tax": "Domestic Standard Rated Tax",
         "Domestic Zero Rated Tax": "Domestic Zero Rated Tax",
@@ -3908,6 +3909,7 @@ var dictionary = {
     },
     ar: {
         "Sub Group Notes:": "ملاحظات المجموعة الفرعية:",
+        "Exports": "الصادرات",
         "Add New Debit Note Register": "إضافة سجل إشعار الخصم الجديد",
         "Add New Proforma Invoice Edit":"هذا هو النموذج الجديد في تحرير الصوت.",
         "ReceiptChildSlNo": "Receipt Child Sl No - رقم تسلسل استلام الفرع",
@@ -6883,6 +6885,7 @@ var dictionary = {
     },
     zh: {
         "Sub Group Notes:": "子组备注：",
+        "Exports": "出口",
         "Add New Debit Note Register": "添加新的借项单登记",
         "Add New Proforma Invoice Edit": "添加新的形式发票编辑",
         "Registers": "登记簿",
@@ -9698,6 +9701,7 @@ var dictionary = {
     },
     hi: {
         "Sub Group Notes:": "उप समूह टिप्पणियाँ:",
+        "Exports": "निर्यात",
         "Add New Debit Note Register": "नया डेबिट नोट रजिस्टर जोड़ें",
         "Add New Proforma Invoice Edit":"यह वॉयस एडिट में एक नया प्रोफार्मा है।",
         "Registers": "रजिस्टर",
@@ -12665,6 +12669,7 @@ var dictionary = {
     },
     ur: {
         "Sub Group Notes:": "ذیلی گروپ نوٹس:",
+        "Exports": "برآمدات",
         "Add New Debit Note Register": "نیا ڈیبٹ نوٹ رجسٹر شامل کریں",
         "Add New Proforma Invoice Edit": "نیا پروفارما انوائس ایڈٹ شامل کریں",
         "Registers": "رجسٹرز",
@@ -15721,6 +15726,7 @@ var dictionary = {
 
     ko: {
         "Sub Group Notes:": "서브 그룹 메모:",
+        "Exports": "수출",
         "Add New Debit Note Register": "새 직불 노트 등록 추가",
         "Add New Proforma Invoice Edit": "새 견적 송장 편집 추가",
         "Registers": "등록부",
