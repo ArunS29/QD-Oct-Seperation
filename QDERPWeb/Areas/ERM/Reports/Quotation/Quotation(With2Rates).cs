173b75185fb3e686f2856c06511066eb20509ffe
SathishCatchu <Satish@quickdice.net>
2025-09-16 15:53:55 +0530

    ERM reports

---
+++ QDERPWeb/Areas/ERM/Reports/Quotation/Quotation(With2Rates).cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace QD.ERP.Web.Areas.ERM.Reports.Quotation
+{
+    public partial class Quotation_With2Rates_ : DevExpress.XtraReports.UI.XtraReport
+    {
+        public Quotation_With2Rates_()
+        {
+            InitializeComponent();
+        }
+    }
+}
